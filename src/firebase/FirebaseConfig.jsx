78e390881d7e6dc894806dc1f4d176189d304cd9
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-07-18 01:31:18 -0400

    commit

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -11,12 +11,12 @@ const firebaseConfig = {
   // appId: "1:484122894175:web:cc885c0a385d469b04a0ea"
 
   
-  apikey: process.env.REACT_APP_FIREBASE_APIKEY_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apikey: process.env.VITE_APP_FIREBASE_APIKEY_KEY,
+  authDomain: process.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_APP_FIREBASE_MESSAGINGSENDER_ID,
+  appId: process.env.VITE_APP_FIREBASE_APP_ID
 
 };
 
