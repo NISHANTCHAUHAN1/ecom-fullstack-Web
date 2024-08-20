d9083b06bacfd8b8f89612e70388e826854fcfca
Nishant Chauhan <144008883+NISHANTCHAUHAN1@users.noreply.github.com>
2024-08-20 04:03:50 -0400

    Update SearchBar.jsx

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -2,44 +2,6 @@ import React, { useContext, useState } from "react";
 import myContext from "../../context/myContext";
 import { useNavigate } from "react-router-dom";
 
-// const searchData = [
-//   {
-//     name: "Fashion",
-//     image:
-//       "https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg",
-//   },
-//   {
-//     name: "Shirt",
-//     image:
-//       "https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg",
-//   },
-//   {
-//     name: "Jacket",
-//     image:
-//       "https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg",
-//   },
-//   {
-//     name: "Mobile",
-//     image:
-//       "https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg",
-//   },
-//   {
-//     name: "Laptop",
-//     image:
-//       "https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg",
-//   },
-//   {
-//     name: "Home",
-//     image:
-//       "https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg",
-//   },
-//   {
-//     name: "book",
-//     image:
-//       "https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg",
-//   },
-// ];
-
 const SearchBar = () => {
 
   const context = useContext(myContext);
